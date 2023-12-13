fbc8fa5a90e71e52b864f2cc278fbc0ba610cae5
Isaac-del23 <bsisaac0323@gmail.com>
2023-12-13 17:33:17 -0300

    Project atualizado

--- src/environment/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-// src/environments/environment.prod.ts
-
-export const environment = {
-    production: true,
-    // Outras configurações específicas do ambiente de produção aqui
-  };
--- src/environment/environment.ts
+++
@@ -1,6 +0,0 @@
-// src/environments/environment.ts
-
-export const environment = {
-    production: false, // Define como true quando estiver em produção
-    // Outras configurações específicas do ambiente aqui
-  };
